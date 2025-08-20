307b6ea2569f534159cb9bd66c9af15de9627c80
zhangjiayi <jiayizhang662@gmail.com>
2025-08-21 00:13:32 +0800

    add increments level-1

--- src/main/java/Broccoli.java
+++ src/main/java/Broccoli.java
@@ -1,20 +1,52 @@
+import java.util.Scanner;
+
 public class Broccoli {
-    public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-        char line = '-';
-        int count = 60;
-        StringBuilder horizontalLine = new StringBuilder();
-        while(count >= 0) {
-            horizontalLine.append(line);
+    private char line = '-';
+    private int count = 60;
+    private StringBuilder horizontalLine;
+    private Scanner scanner;
+
+    public Broccoli() {
+        this.line = '-';
+        this.count = 60;
+        this.scanner = new Scanner(System.in);   // give access to whatever the user type
+        this.horizontalLine = new StringBuilder();
+    }
+
+    public void setHorizontalLine() {
+        while(this.count >= 0) {
+            this.horizontalLine.append(line);
             count --;
         }
-        System.out.println(horizontalLine.toString());
+        System.out.println(this.horizontalLine.toString());
+    }
+
+    public void greeting() {
         System.out.println("Hello! I'm Broccoli\n" + "What can I do for you?\n" + horizontalLine.toString());
+    }
+
+    public void exiting() {
         System.out.println("Bye.Hope to see you again soon!\n" + horizontalLine.toString());
+    }
+
+    public void echo() {
+        String task = "";
+        while(true) {
+            task = scanner.nextLine();
+            if(task.equals("bye")) {
+                exiting();
+                break;
+            }
+            System.out.println(horizontalLine.toString());
+            System.out.println(task);
+            System.out.println(horizontalLine.toString());
+
+        }
+    }
+    public static void main(String[] args) {
+     Broccoli broccoli = new Broccoli();
+     broccoli.setHorizontalLine();
+     broccoli.greeting();
+     broccoli.echo();
     }
 }
